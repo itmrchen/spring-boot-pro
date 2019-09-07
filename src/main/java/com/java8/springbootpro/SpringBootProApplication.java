f53ca1063454e340d37b86304b8959e5f9ed2fb0
chenbo <chenbo6@wejoydata.com>
2019-09-08 00:24:44 +0800

    Initial commit

---
+++ src/main/java/com/java8/springbootpro/SpringBootProApplication.java
@@ -0,0 +1,13 @@
+package com.java8.springbootpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProApplication.class, args);
+    }
+
+}
