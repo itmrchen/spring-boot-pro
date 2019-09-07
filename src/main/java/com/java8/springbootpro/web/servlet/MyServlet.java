75604feeb832dc2b686b13f64f29263d6e249573
chenbo <chenbo6@wejoydata.com>
2019-09-08 02:32:08 +0800

    spring-boot 实现servlet的异步方式

--- src/main/java/com/java8/springbootpro/web/servlet/MyServlet.java
+++ src/main/java/com/java8/springbootpro/web/servlet/MyServlet.java
@@ -1,5 +1,6 @@
 package com.java8.springbootpro.web.servlet;
 
+import javax.servlet.AsyncContext;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,11 +13,21 @@ import java.io.IOException;
  * @author itmrchen
  * @date 2019/9/8 0:34
  */
-@WebServlet(urlPatterns = "/my/servlet")
+@WebServlet(urlPatterns = "/my/servlet", asyncSupported = true)
 public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.getWriter().print("hello spring boot!");
+        AsyncContext asyncContext = req.startAsync();
+        asyncContext.start(() -> {
+            try {
+                resp.getWriter().print("hello spring boot!");
+                // 需要主动完成
+                asyncContext.complete();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
 
     }
 }
