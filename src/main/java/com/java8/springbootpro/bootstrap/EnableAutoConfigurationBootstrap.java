c54e63fe2e162f2accc830237d8cc10772fea7e4
chenbo <chenbo6@wejoydata.com>
2019-09-11 01:40:37 +0800

    spring-boot 自动装配

---
+++ src/main/java/com/java8/springbootpro/bootstrap/EnableAutoConfigurationBootstrap.java
@@ -0,0 +1,20 @@
+package com.java8.springbootpro.bootstrap;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * @author itmrchen
+ * @date 2019/9/11 1:18
+ */
+@EnableAutoConfiguration
+public class EnableAutoConfigurationBootstrap {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = new SpringApplicationBuilder(EnableAutoConfigurationBootstrap.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+        context.close();
+    }
+}
---
+++ src/main/java/com/java8/springbootpro/configuration/HelloWorldAutoConfiguration.java
@@ -0,0 +1,13 @@
+package com.java8.springbootpro.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * helloworld自动装配
+ *
+ * @author itmrchen
+ * @date 2019/9/11 1:21
+ */
+@Configuration
+public class HelloWorldAutoConfiguration {
+}
