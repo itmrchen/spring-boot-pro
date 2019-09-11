494d2f5ef1e34d4a51265c9c09aeb892632cee32
itmrchen <810890@163.com>
2019-09-12 01:09:50 +0800

    spring-boot引导类另外一种写法

--- spring-application/src/main/java/com/java8/diveinspringboot/SpringApplicationBootStrap.java
+++ spring-application/src/main/java/com/java8/diveinspringboot/SpringApplicationBootStrap.java
@@ -9,10 +9,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author itmrchen
  * @date 2019/9/12 0:42
  */
-@SpringBootApplication
+//第二种写法 注释掉下面
+//@SpringBootApplication
 public class SpringApplicationBootStrap {
     public static void main(String[] args) {
-        SpringApplication.run(SpringApplicationBootStrap.class, args);
+        //SpringApplication.run(SpringApplicationBootStrap.class, args);
+        SpringApplication.run(ApplicationConfiguration.class, args);
+    }
+
+    @SpringBootApplication
+    public static class ApplicationConfiguration {
     }
 
 }
