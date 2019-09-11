0ae61dfc0013b195394b697aa5c3d3edb2144ef0
chenbo <chenbo6@wejoydata.com>
2019-09-12 01:00:56 +0800

    spring-boot引导类

---
+++ spring-application/src/main/java/com/java8/diveinspringboot/SpringApplicationBootStrap.java
@@ -0,0 +1,18 @@
+package com.java8.diveinspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * {@link SpringApplication}引导类
+ *
+ * @author itmrchen
+ * @date 2019/9/12 0:42
+ */
+@SpringBootApplication
+public class SpringApplicationBootStrap {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplicationBootStrap.class, args);
+    }
+
+}
